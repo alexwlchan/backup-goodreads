4d946b46ed79b76019d6796c02d803f25452295b
Alex Chan <alex@alexwlchan.net>
2017-02-07 18:58:41 +0000

    Initial commit: original backup goodreads script

---
+++ backup_goodreads.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+"""A script for backing up your Goodreads library.  Writes a backup to
+`goodreads_backup.json`, or you can provide an alternative name as the first
+command-line argument.
+
+See the README for more details.
+
+"""
+
+from __future__ import print_function
+
+import itertools
+import sys
+from datetime import datetime, timezone
+from xml.etree import ElementTree as ET
+
+import requests
+
+if sys.version_info < (3, 0):
+    print('This script must be run in Python 3.', file=sys.stderr)
+    sys.exit(1)
+
+
+def identity(x):
+    """Identity function."""
+    return x.text
+
+
+def convert_date(element):
+    """Convert a date string from the Goodreads API into an ISO-8601 UTC
+    string."""
+    # We may get ``None`` if the API is missing any information for a
+    # particular date field -- for example, the ``date_read`` field is
+    # only filled in when a book is read.
+    date_str = element.text
+    if date_str is None:
+        return None
+    else:
+        # In the API responses, dates are returned in the form
+        # "Mon Oct 24 12:26:31 -0700 2016"
+        date_obj = datetime.strptime(date_str, '%a %b %d %H:%M:%S %z %Y')
+        return str(date_obj.astimezone(timezone.utc))
+
+
+def convert_rating(element):
+    """Convert a rating from the Goodreads API."""
+    rating = element.text
+
+    # The Goodreads API returns '0' to indicate an unrated book; make
+    # this a proper null type.
+    if rating == '0':
+        return None
+    else:
+        return rating
+
+
+def convert_authors(element):
+    """Get the names of the authors for this book."""
+    # The Goodreads API returns information about authors in an <authors>
+    # element on the <review> element, in the following format:
+    #
+    #    <authors>
+    #      <author>
+    #        <id>1234</id>
+    #        <name>John Smith</name>
+    #        <snip>...</snip>
+    #      </author>
+    #      <author>
+    #        <id>5678</id>
+    #        <name>Jane Doe</name>
+    #        <snip>...</snip>
+    #      </author>
+    #    </authors>
+    #
+    return [author.find('name').text for author in element.findall('author')]
+
+
+def convert_shelves(element):
+    """Get the names of all the shelves that hold this book."""
+    # The shelves for a review are returned in the following format:
+    #
+    #    <shelves>
+    #      <shelf name="read" exclusive="true"/>
+    #      <shelf name="fiction" exclusive="false" review_shelf_id="1234"/>
+    #    </shelves>
+    #
+    return [shelf.attrib['name'] for shelf in element.findall('shelf')]
+
+
+def convert_page_count(element):
+    try:
+        return int(element.text)
+    except TypeError:
+        return None
+
+
+def convert_body(element):
+    return element.text.strip()
+
+
+# Map from <review> tag name to pair (output key, converter)
+REVIEW_TAGS = {
+    'read_at':      ('date_read',   convert_date),
+    'date_added':   ('date_added',  convert_date),
+    'body':         ('review',      convert_body),
+    'rating':       ('my_rating',   convert_rating),
+    'shelves':      ('bookshelves', convert_shelves)
+}
+
+# Map from <book> tag name to pair (output key, converter)
+BOOK_TAGS = {
+    'authors':          ('authors',             convert_authors),
+    'id':               ('book_id',             identity),
+    'title':            ('title',               identity),
+    'isbn':             ('isbn',                identity),
+    'isbn13':           ('isbn13',              identity),
+    'average_rating':   ('average_rating',      identity),
+    'publisher':        ('publisher',           identity),
+    'format':           ('binding',             identity),
+    'num_pages':        ('page_count',          convert_page_count),
+    'publication_year': ('publication_year',    identity),
+    'published':        ('orig_year_published', identity),
+}
+
+
+def _get_data_from_goodreads_api(user_id, api_key, page_no):
+    """Retrieve data about the reviews of a given user from the Goodreads API.
+    Returns a Request object if successful, raises an Exception if not.
+
+    :param user_id: The ID of the Goodreads user.
+    :param api_key: The Goodreads API key.
+    :param page_no: Which page of reviews to fetch.
+
+    """
+    # reviews.list (https://www.goodreads.com/api/index#reviews.list) gets
+    # all the books on somebody's shelf.
+    req = requests.get('https://www.goodreads.com/review/list.xml', params={
+        'v': '2',
+        'key': api_key,
+        'id': user_id,
+        'page': str(page_no)
+    })
+    if req.status_code != 200:
+        raise Exception(
+            'Unexpected error code from Goodreads API: %s\n'
+            'Error message: %r' % (req.status_code, req.text)
+        )
+    return req
+
+
+def _get_reviews_from_api(user_id, api_key):
+    """Generates <review> elements from the API data.
+
+    :param user_id: The ID of the Goodreads user.
+    :param api_key: The Goodreads API key.
+
+    """
+    for page_no in itertools.count(1):
+        req = _get_data_from_goodreads_api(
+            user_id=user_id, api_key=api_key, page_no=page_no
+        )
+        reviews = ET.fromstring(req.text).find('reviews')
+        yield from reviews
+        if int(reviews.attrib['end']) >= int(reviews.attrib['total']):
+            break
+
+
+def get_reviews(user_id, api_key):
+    """Generate reviews associated with a Goodreads user as dictionaries.
+
+    :param user_id: The ID of the Goodreads user.
+    :param api_key: The Goodreads API key.
+
+    """
+    for review in _get_reviews_from_api(user_id, api_key=api_key):
+        data = {}
+
+        def convert(element, tag_mapping):
+            if element.tag in tag_mapping:
+                key, converter = tag_mapping[element.tag]
+                data[key] = converter(element)
+
+        for review_elt in review:
+            if review_elt.tag == 'book':
+                for book_elt in review_elt:
+                    convert(book_elt, BOOK_TAGS)
+            else:
+                convert(review_elt, REVIEW_TAGS)
+        yield data
+
+
+def main():
+    """Parse the Goodreads API and save the reviews to disk."""
+    try:
+        path = sys.argv[1]
+    except IndexError:
+        path = 'goodreads_backup.json'
+
+    USER_ID = keyring.get_password('goodreads', 'user_id')
+    API_KEY = keyring.get_password('goodreads', 'api_key')
+
+    reviews = get_reviews(user_id=USER_ID, api_key=API_KEY)
+    json_str = json.dumps(list(reviews), indent=2, sort_keys=True)
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(json_str)
+    print('Written backup to %s' % path)
+
+
+if __name__ == '__main__':
+    import json
+    import keyring  # noqa: F401
+    main()
